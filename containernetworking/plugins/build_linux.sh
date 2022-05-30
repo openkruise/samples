c2d59d0bf1b46b0c93727b44f490111363c81bfb
liheng.zms <liheng.zms@alibaba-inc.com>
2022-05-30 11:34:19 +0800

    add reserved ip host-local
    Signed-off-by: liheng.zms <liheng.zms@alibaba-inc.com>

---
+++ containernetworking/plugins/build_linux.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+cd "$(dirname "$0")"
+
+if [ "$(uname)" == "Darwin" ]; then
+	export GOOS="${GOOS:-linux}"
+fi
+
+export GOFLAGS="${GOFLAGS} -mod=vendor"
+
+mkdir -p "${PWD}/bin"
+
+echo "Building plugins ${GOOS}"
+PLUGINS="plugins/ipam/host-local"
+for d in $PLUGINS; do
+	if [ -d "$d" ]; then
+		plugin="$(basename "$d")"
+		if [ "${plugin}" != "windows" ]; then
+			echo "  $plugin"
+			${GO:-go} build -o "${PWD}/bin/$plugin" "$@" ./"$d"
+		fi
+	fi
+done
